28eec7190494c472c4700bdcea25f2d5edb119eb
Jaydee White <jddubble2@gmail.com>
2026-03-05 08:33:07 -0700

    Chore: Added backend & iOS

---
+++ tabletviewTemp/tabletView/tabletViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabletViewApp.swift
+//  tabletView
+//
+//  Created by Pwurple on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct tabletViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
